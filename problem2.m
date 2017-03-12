866aaa288f0d694da1df36bf12a79cbf4ea62e84
Jakub Czerny <Jakub Czerny>
2017-03-12 20:46:58 +0100

    8 first questions of Problem 2

--- problem2.m
+++ problem2.m
@@ -1,8 +1,8 @@
 u_bar = 0.2;
 d_0 = 1;
 n = 5;
-
-EqualityQPSolverLDL(n,u_bar,d_0)
-EqualityQPSolverLU(n,u_bar,d_0)
-NullSpaceQR(n,u_bar,d_0)
-RangeSpaceSolver(n,u_bar,d_0)
+clc
+[x, lambda] = EqualityQPSolverLDL(n,u_bar,d_0)
+[x, lambda] = EqualityQPSolverLU(n,u_bar,d_0)
+[x, lambda] = NullSpaceQR(n,u_bar,d_0)
+[x, lambda] = RangeSpaceSolver(n,u_bar,d_0)
