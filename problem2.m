bee19dbe59ae41cd92ca2e55dd992bf5fdb9f647
Jakub Czerny <Jakub Czerny>
2017-03-12 21:48:24 +0100

    9 first questions of Problem 2

--- problem2.m
+++ problem2.m
@@ -1,8 +1,57 @@
+clear all
+clc 
+close all
+
 u_bar = 0.2;
 d_0 = 1;
-n = 5;
+% n = 5;
 clc
-[x, lambda] = EqualityQPSolverLDL(n,u_bar,d_0)
-[x, lambda] = EqualityQPSolverLU(n,u_bar,d_0)
-[x, lambda] = NullSpaceQR(n,u_bar,d_0)
-[x, lambda] = RangeSpaceSolver(n,u_bar,d_0)
+
+ns = [10:10:1000];
+times = zeros(4, size(ns,2));
+
+name = 'Data/problem_2-question_9.mat';
+if exist(name, 'file') 
+    disp('Loading data..')
+    load(name)
+else
+
+    disp('Iterating...')
+    i = 1;
+    for n = ns
+
+        % Print n every 100 iterations
+        if mod(n,100) == 0
+            str = sprintf('n = %d', n);
+            disp(str)
+        end
+
+        start_time = cputime;
+        [x, lambda] = EqualityQPSolverLDL(n,u_bar,d_0);
+        times(1,i) = cputime-start_time;
+
+        start_time = cputime;
+        [x, lambda] = EqualityQPSolverLU(n,u_bar,d_0);
+        times(2,i) = cputime-start_time;
+
+        start_time = cputime;
+        [x, lambda] = NullSpaceQR(n,u_bar,d_0);
+        times(3,i) = cputime-start_time;
+
+        start_time = cputime;
+        [x, lambda] = RangeSpaceSolver(n,u_bar,d_0);
+        times(4,i) = cputime-start_time;    
+
+        i = i + 1;
+    end
+    save(name, 'times')
+end
+disp('Finished')
+
+% Geranate plots
+plot(ns,times(1,:), 'ro')
+hold on
+plot(ns,times(2,:), 'bs')
+plot(ns,times(3,:), 'g*')
+plot(ns,times(4,:), 'k.')
+legend('LDL solver','LU solver','Null Space solver','Range Space solver')
