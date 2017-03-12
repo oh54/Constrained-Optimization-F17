a6f27f858a5000c9fc5efd7a27358f5cff5fd5e5
Jakub Czerny <Jakub Czerny>
2017-03-12 20:43:43 +0100

    Added missing index correction

--- EqualityQPSolverLDL.m
+++ EqualityQPSolverLDL.m
@@ -4,8 +4,9 @@ function [x, lambda] = EqualityQPSolverLDL(n, u_bar, d_0)
     m = size(A,2);
     
     [L,D,p] = ldl(KKT_A,'lower','vector');
-    KKT_x = L'\(D\(L\KKT_b(p)));
-
+    KKT_x(p) = L'\(D\(L\KKT_b(p)));
+    KKT_x = KKT_x';
+    
     x = KKT_x(1:n);
     lambda = KKT_x(n+1:n+m); 
 end
