6af29ea301298300a02ccd203a463be75e343760
Jakub Czerny <Jakub Czerny>
2017-03-12 20:41:14 +0100

    Fixed NullSpace solver

--- RangeSpaceSolver.m
+++ RangeSpaceSolver.m
@@ -4,14 +4,20 @@ function [x, lambda] = RangeSpaceSolver(n, u_bar, d_0)
     n = size(A,1);
     m = size(A,2);  
     
-    L = chol(H,'lower');
-    v = H\g;
+    % Hv = g
+    [L,~,s] = chol(sparse(H),'lower','vector');
+    v = L'\(L\g(s));
     
-    ro = 1/(v'*g);
-    H_A = ( eye(n,n) - ro * v * g') * ( eye(n,n) - ro * g * v' ) + ro * v * v'; 
-    L_A = chol(H_A,'lower');
+    % inv(H) ~ H
+    H_A = A'*H*A;
     
-    
-    lambda = H_A \ ( b + A'*v);
-    x = H \ (A*lambda - g);
+    % H_A*lambda = b + A'*v
+    [L,~,s] = chol(sparse(H_A),'lower','vector');
+    tmp = b + A'*v;
+    lambda(s) = L'\(L\tmp(s));
+    lambda = lambda';
+
+    % H*x = A*lambda - g
+    tmp = A*lambda - g;
+    x = H \ tmp;
 end
