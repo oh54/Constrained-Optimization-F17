df7063298bb9682f85ec5a5b911dfdb139c7b473
JakubCzerny <kuba.czerny@poczta.fm>
2017-03-14 18:05:21 +0100

    Testing interior-point algorithm with equality constraints

--- problem5.m
+++ problem5.m
@@ -1,1 +1,14 @@
-%hello
+u_bar = 0.2;
+d_0 = 1;
+n = 5;
+clc
+[x, lambda] = EqualityQPSolverLDL(n,u_bar,d_0)
+
+[KKT_A, KKT_b, H, g, A, b] = ConstructKKT(n, u_bar, d_0);
+[m,n] = size(A);
+C = zeros(m,n);
+d = zeros(n,1);
+
+[x, y, z, s] = PrimalDualInteriorPointSolver(H, g, A, b, C, d)
+
+
