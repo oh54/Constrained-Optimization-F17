e547a8f64c73bc8b1a4cdc573becc7c7db12180a
JakubCzerny <kuba.czerny@poczta.fm>
2017-03-14 18:01:46 +0100

    Working Primal-Dual Interior Point solver (requires cleaning and optimization

--- PrimalDualInteriorPointSolver.m
+++ PrimalDualInteriorPointSolver.m
@@ -1,32 +1,56 @@
-function [x, lambda] = PrimalDualInteriorPointSolver(H, g, A, b, C, d, x, y, Z, S)
-    m = size(s,1);
-    e = ones(m,1);
-    k = size(A,1);
-    l = size(A,2);
+function [x, y, z, s] = PrimalDualInteriorPointSolver(H, g, A, b, C, d)
+    [o,p] = size(C);
+    [k,l] = size(A);
+    [q,r] = size(d)
+    
+    % Initialization of parameters 
+    x = ones(k,1);
+    y = ones(l,1);
+    s = ones(q,1);
+    z = ones(q,1);
+    
+    Z = diag(z);
+    S = diag(s);
+        
+    [m,n] = size(S);
+    e = ones(m,1);    
     eta = 0.995;
     
-    max_iter = 1000;
-    eps = 10^-5;
+    max_iter = 100  ;
+    tol = 10^-9;
     
     % Compute residuals
-    rL = H*x + g - A*y - C*Z;
+    rL = H*x + g - A*y - C*z;
     rA = b - A'*x;
     rC = s + d - C'*x;
     rSZ= Z*S*e; 
     mu = z'*s/m;
             
+%     disp('-----------')
+%     disp(norm(rL,inf))
+%     disp(norm(rA,inf))
+%     disp(norm(rC,inf))
+%     disp(abs(mu))
+%     disp('-----------')
+    
+
+    Converged = (norm(rL,inf) <= tol) && ...
+                (norm(rA,inf) <= tol) && ...
+                (norm(rC,inf) <= tol) && ...
+                (abs(mu) <= tol);
     i = 1;
-    
-    % Get norms of residuals
-    norm_L = norm(rL,inf);
-    norm_A = norm(rA,inf);
-    norm_C = norm(rC,inf);
-    
-    while i<max_iter & norm_L <= eps*max(1,normL) & norm_C <= eps*max(1,norm_C) & norm_A <= eps*max(1,norm_A) & abs(mu) > eps_mu   
-              
+%%        
+    while ~Converged && (i<max_iter)
+%         disp(i)
         % LDL factorization of modified KKT
-        inv_S = pinv(S);
-        inv_Z = pinv(Z);
+        s = diag(S);
+        z = diag(Z);
+        
+        % TEMPORARILY FOR GETTING IT TO WORK
+        inv_S = inv(S);
+        inv_Z = inv(Z);
+        
+        
         H = H + C*(inv_S*Z)*C';
         KKT = [H, -A; -A', zeros(l,l)];
         [L,D,p] = ldl(KKT,'lower','vector');
@@ -39,16 +63,18 @@ function [x, lambda] = PrimalDualInteriorPointSolver(H, g, A, b, C, d, x, y, Z, 
         % Calculate affine deltas
         tmp = -[rL_bar;rA];
         X(p) = L'\(D\(L\tmp(p)));
+        X = reshape(X, length(X), 1);
         dX_aff = X(1:k);
         dY_aff = X(k+1:k+l);
         
-        tmp = inv_S*Z
+        tmp = inv_S*Z;
         dZ_aff = -tmp * C' * dX_aff + tmp * (rC - inv_Z * rSZ);
-        dS_aff = -inv_Z * rSC - inv_Z * S * dZ_aff;
+        dS_aff = -inv_Z * rSZ - inv_Z * S * dZ_aff;
         
         % Compute largest alpha_aff
-        alpha_aff = 1;
-        % >> To be implemented <<
+        idxZ = find(dZ_aff < 0.0);
+        idxS = find(dS_aff < 0.0);   
+        alpha_aff = min([1.0; -z(idxZ,1)./dZ_aff(idxZ,1); -s(idxS,1)./dS_aff(idxS,1)]);
 
         % Dual gap
         mu = z'*s/m;
@@ -59,44 +85,69 @@ function [x, lambda] = PrimalDualInteriorPointSolver(H, g, A, b, C, d, x, y, Z, 
         %% CENTERING STEP
         
         % Update residuals
-        rSZ_bar= rSZ + dS_aff * dZ_aff*e - ro*mu*e;
+        rSZ_bar= rSZ + dS_aff' * dZ_aff * e - ro * mu * e;
         rL_bar = rL - C * (inv_S*Z) * (rC-inv_Z*rSZ_bar);
         
         % Calculate centering deltas
         tmp = -[rL_bar;rA];
+        X = [];
         X(p) = L'\(D\(L\tmp(p)));
+        X = reshape(X, length(X), 1);
         dX_cen = X(1:k);
         dY_cen = X(k+1:k+l);
         dZ_cen = - (inv_S*Z) * C' * dX_cen + (inv_S*Z)*(rC - inv_Z * rSZ_bar);
-        dS_cen = - inv_Z * rSC_bar - inv_Z * S * dZ_cen;
+        dS_cen = - inv_Z * rSZ_bar - inv_Z * S * dZ_cen;
         
         
         % Compute largest alpha_cen
-        alpha_cen = 1;
-        % >> To be implemented <<
-        
+        idxZ = find(dZ_cen < 0.0);
+        idxS = find(dS_cen < 0.0);   
+        alpha_cen = min([1.0; -z(idxZ,1)./dZ_cen(idxZ,1); -s(idxS,1)./dS_cen(idxS,1)]);
         alpha = alpha_cen * eta;
-               
+                       
         % Update position
         x = x + alpha * dX_cen;
         y = y + alpha * dY_cen;
         z = z + alpha * dZ_cen;
         s = s + alpha * dS_cen;
-                
+       
+        Z = diag(z);
+        S = diag(s);        
+        
         % Update residuals
-        rL = H*x + g - A*y - C*Z;
+        rL = H*x + g - A*y - C*z;
         rA = b - A'*x;
         rC = s + d - C'*x;
         rSZ= Z*S*e; 
+        mu = z'*s/m;
         
         % Get norms of residuals
-        norm_L = norm(rL,inf);
-        norm_A = norm(rA,inf);
-        norm_C = norm(rC,inf);
+%         disp('-----------')
+%         disp(norm(rL,inf))
+%         disp(norm(rA,inf))
+%         disp(norm(rC,inf))
+%         disp(abs(mu))
+%         disp('-----------')
+
+        Converged = (norm(rL,inf) <= tol) && ...
+                    (norm(rA,inf) <= tol) && ...
+                    (norm(rC,inf) <= tol) && ...
+                    (abs(mu) <= tol);
     
         i = i + 1;
     end
-    
-    x = 0;
-    lambda = 0;
+
+%         disp('-----------')
+%         disp(norm(rL,inf))
+%         disp(norm(rA,inf))
+%         disp(norm(rC,inf))
+%         disp(abs(mu))
+%         disp('-----------')
+
+        if ~Converged
+            x=[];
+            y=[];
+            z=[];
+            s=[];
+        end
 end
