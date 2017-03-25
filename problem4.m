ca64f7e6191d1aab8bce23801f9092f68c9e96be
oskar <oskar.hint@gmail.com>
2017-03-25 23:01:13 +0100

    Problem 4 code

--- problem4.m
+++ problem4.m
@@ -1,9 +1,9 @@
 % problem 4 - Markowitz
-H = [ 2.30 0.93 0.62 0.74 -0.23 ; 0.93 1.40 0.22 0.56 0.26 ; 0.62 0.22 1.80 0.78 -0.27 ; 0.74 0.56 0.78 3.40 -0.56 ;-0.23 0.26 -0.27 -0.56 2.60 ];
+cov = [ 2.30 0.93 0.62 0.74 -0.23 ; 0.93 1.40 0.22 0.56 0.26 ; 0.62 0.22 1.80 0.78 -0.27 ; 0.74 0.56 0.78 3.40 -0.56 ;-0.23 0.26 -0.27 -0.56 2.60 ];
 returns = [ 15.10 ; 12.50 ; 14.70 ; 9.02 ; 17.68 ];
 
-H = H .* 2;
-
+H = cov .* 2;
+%H = cov;
 % Q1
 % objective function should minimize risk
 % there are two equality constraints, one so that return = 10 and the other
@@ -14,10 +14,10 @@ H = H .* 2;
 % that all weights must be larger/equal to zero
 
 f = [ 0 ; 0 ; 0 ; 0 ; 0 ];
-A = [];
-b = [];
-Aeq = [ 1 1 1 1 1 ; returns' ];
-beq = [ 1 ; 10 ];
+A = -returns';
+b = -10;
+Aeq = [ 1 1 1 1 1 ];
+beq = 1;
 lb = [ 0 ; 0 ; 0 ; 0 ; 0 ];
 ub = [ 1 ; 1 ; 1 ; 1 ; 1 ];
 % Q2
@@ -26,102 +26,105 @@ ub = [ 1 ; 1 ; 1 ; 1 ; 1 ];
 % Q3
 %[x,fval,exitflag,output,lambda] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
 % R = 10 portfolio
-beq = [ 1 ; 17 ];
-[x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
-x_17 = x;
-fval_17 = fval;
-
-% R = 17
 [x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
 x_10 = x;
 fval_10 = fval;
 
+% R = 17
+b = -17;
+[x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
+x_17 = x;
+fval_17 = fval;
+
+b = -17.5;
+[x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
+x_175 = x;
+fval_175 = fval;
+
 
 % min variance portfolio
-Aeq = [ 1 1 1 1 1 ];
-beq = 1;
+b = 0;
 [x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
 x_min = x;
 fval_min = fval;
 
 % Q4
-% The efficient frontier can be traced out as a combinations of any two
-% efficient portfolios
-
-%plot_variances = zeros(21, 1);
 plot_variances = [];
-%plot_returns = zeros(21, 1);
 plot_returns = [];
-%i = 0
-for alpha = -2:0.05:2
-    %i = i+1;
-    beta = 1 - alpha;
-    xvec = x_17 .* alpha + x_min .* beta;
-    
-    %plot_returns(i) = returns' * xvec;
-    %plot_variances(i) = xvec' * H * xvec;
-    
-    plot_returns = [plot_returns, returns' * xvec];
-    plot_variances = [plot_variances, xvec' * H * xvec];
-end
+for R = 9:0.05:17.70
+    b = -R;
+    [x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);    
 
-figure();
-plot(plot_variances, plot_returns);
-xlim([0, 8]);
+    plot_returns = [plot_returns, x' * returns];
+    plot_variances = [plot_variances, fval];
+
+end
+p1 = plot(plot_variances, plot_returns, 'LineWidth', 2);
+ylim([8, 21]);
+xlim([0, 4]);
+title('Efficient frontier');
+xlabel('Risk (portfolio variance)');
+ylabel('Return (%)');
 hold on;
+%scatter(fval_10, x_10' * returns, 20); 
+
+%txt1 = '\leftarrow Minimum variance portfolio';
+%text(fval_10 + 0.05, x_10' * returns + 0.03, txt1)
 
 % Q5
-H = [ 2.30 0.93 0.62 0.74 -0.23 ; 0.93 1.40 0.22 0.56 0.26 ; 0.62 0.22 1.80 0.78 -0.27 ; 0.74 0.56 0.78 3.40 -0.56 ;-0.23 0.26 -0.27 -0.56 2.60 ];
 returns = [ 15.10 ; 12.50 ; 14.70 ; 9.02 ; 17.68 ; 2.0 ];
-
-% add a column and row of zeros to H, representing that the riskfree
+% add a column and row of zeros to the covariance matrix, representing that the riskfree
 % security isn't correlated with any of the previous securities
-H = [H, zeros(5,1)];
-H = [H ; zeros(1,6)];
+cov = [cov, zeros(5,1)];
+cov = [cov ; zeros(1,6)];
+H = 2 .* cov;
 
 f = [ 0 ; 0 ; 0 ; 0 ; 0 ; 0];
 lb = [ 0 ; 0 ; 0 ; 0 ; 0 ; 0];
 ub = [ 1 ; 1 ; 1 ; 1 ; 1 ; 1];
 
 % Q6
-
 Aeq = [ 1 1 1 1 1 1 ];
 beq = 1;
-[x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
-x_min_rf = x;
-fval_min_rf = fval;
-
-
-Aeq = [ 1 1 1 1 1 1 ; returns' ];
-beq = [ 1 ; 10 ];
-[x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
-x_10_rf = x;
-fval_10_rf = fval;
-
+A = -returns';
 
 plot_variances = [];
 %plot_returns = zeros(21, 1);
 plot_returns = [];
-%i = 0
-for alpha = -100:0.05:100
-    %i = i+1;
-    beta = 1 - alpha;
-    xvec = x_10_rf .* alpha + x_min_rf .* beta;
-    
-    %plot_returns(i) = returns' * xvec;
-    %plot_variances(i) = xvec' * H * xvec;
-    
-    plot_returns = [plot_returns, returns' * xvec];
-    plot_variances = [plot_variances, xvec' * H * xvec];
+for R = 7:0.05:17.70
+    b = -R;
+    [x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);    
+
+    plot_returns = [plot_returns, x' * returns];
+    plot_variances = [plot_variances, fval];
+
 end
 
-plot(plot_variances, plot_returns, 'g');
-%xlim([0, 8]);
+hold on;
+p2 = plot(plot_variances, plot_returns, 'LineWidth', 2);
+
+hold on;
+vars = diag(cov);
+
+for i = 1:5
+   s = scatter(vars(i), returns(i), 20); 
+   txt = int2str(i);
+   text(vars(i)+0.1, returns(i), txt);
+    
+end
+
+
+legend([p1 p2],{'Initial','With risk free asset'});
 
 % Q7
-beq = [ 1 ; 15 ];
-[x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);
+b = -15;
+[x,fval] = quadprog(H, f, A, b, Aeq, beq, lb, ub);  
 x_15_rf = x;
 fval_15_rf = fval;
+r_15_rf = returns' * x_15_rf;
 
+hold on;
+p_15 = scatter(fval_15_rf, r_15_rf, 20); 
+txt15 = '\leftarrow P_{15}';
+text(fval_15_rf + 0.1, r_15_rf -0.05, txt15);
 
